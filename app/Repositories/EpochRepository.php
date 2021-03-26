4ad3f0415eabf2c8c3393e29ae40df3e40d5af7a
zashton <17747090+zashton@users.noreply.github.com>
2021-03-26 09:47:33 +0800

    fix opt out issue

--- app/Repositories/EpochRepository.php
+++ app/Repositories/EpochRepository.php
@@ -86,7 +86,7 @@ class EpochRepository
     }
 
     public function removeAllPendingGiftsReceived($user, $updateData = []) {
-        $pendingGifts = $user->pendingReceivedGifts();
+        $pendingGifts = $user->pendingReceivedGifts;
         DB::transaction(function () use ($user, $updateData, $pendingGifts) {
            if(!empty($updateData['non_receiver']) && $updateData['non_receiver'] != $user->non_receiver && $updateData['non_receiver'] == 1)
            {
