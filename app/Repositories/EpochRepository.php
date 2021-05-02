df53845d8d7202570f446f07f3918f6062d2d8b3
zashton <17747090+zashton@users.noreply.github.com>
2021-05-02 08:10:07 +0800

    fix typo in update

--- app/Repositories/EpochRepository.php
+++ app/Repositories/EpochRepository.php
@@ -203,7 +203,7 @@ class EpochRepository
                 $circle = $epoch->circle;
                 $unalloc_users = $circle->users()->where('non_giver',0)->where('is_hidden',0)->where('give_token_remaining','>',0)->get();
                 $circle->notify(new EpochAlmostEnd($unalloc_users));
-                $epoch->notified_end = Carbon::now();
+                $epoch->notified_before_end = Carbon::now();
                 $epoch->save();
             }
         }
