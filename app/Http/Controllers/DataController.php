9dd9e98378752d16591bf5d4f973d06e7a04f960
zashton <17747090+zashton@users.noreply.github.com>
2021-06-15 21:20:35 +0800

    remove redundant value

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -118,7 +118,7 @@ class DataController extends Controller
         if(!$user)
             return response()->json(['error'=> 'Address not found'],422);
 
-        $data = $request->only('name','address','non_receiver','bio','epoch_first_visit','regift_percent');
+        $data = $request->only('name','address','non_receiver','bio','epoch_first_visit');
         $data['address'] =  strtolower($data['address']);
         $user = $this->repo->removeAllPendingGiftsReceived($user, $data);
         return response()->json($user);
