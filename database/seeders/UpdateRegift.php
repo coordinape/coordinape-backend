a1a82f6e3d18a2af219be9d02a0072bda82d6fff
zashton <17747090+zashton@users.noreply.github.com>
2021-06-16 00:59:38 +0800

    regift script

--- database/seeders/UpdateRegift.php
+++ database/seeders/UpdateRegift.php
@@ -12,8 +12,29 @@ class UpdateRegift extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
-        User::where('non_receiver',1)->update(['regift_percent'=>100]);
+        $users = User::with('pendingReceivedGifts')->where('regift_percent',100)->get();
+        foreach($users as $user) {
+            $pendingGifts = $user->pendingReceivedGifts;
+            if(count($pendingGifts)) {
+                $pendingGifts->load(['sender.pendingSentGifts']);
+                foreach($pendingGifts as $gift) {
+                    if(!$gift->tokens && $gift->note)
+                        continue;
+
+                    $sender = $gift->sender;
+                    $gift_token = $gift->tokens;
+                    $gift->delete();
+                    $token_used = $sender->pendingSentGifts->SUM('tokens') - $gift_token;
+                    $sender->give_token_remaining = $sender->starting_tokens - $token_used;
+                    $sender->save();
+                }
+                $updateData['give_token_received'] = 0;
+            }
+            $updateData['non_receiver'] = 1;
+            $user->update($updateData);
+        }
     }
 }
