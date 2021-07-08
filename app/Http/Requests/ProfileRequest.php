13ded698d1c6f819f434dc560c74d26c2dc83b85
zashton <17747090+zashton@users.noreply.github.com>
2021-07-09 03:19:39 +0800

    add twitter username in request

--- app/Http/Requests/ProfileRequest.php
+++ app/Http/Requests/ProfileRequest.php
@@ -36,6 +36,7 @@ class ProfileRequest extends FormRequest
             'github_username' => !empty($data['github_username']) ? $data['github_username']:null,
             'telegram_username' => !empty($data['telegram_username']) ? $data['telegram_username']:null,
             'discord_username' => !empty($data['discord_username']) ? $data['discord_username']:null,
+            'twitter_username' => !empty($data['twitter_username']) ? $data['twitter_username']:null,
             'medium_username' => !empty($data['medium_username']) ? $data['medium_username']:null,
             'website' => !empty($data['website']) ? $data['website']:null
         ]);
