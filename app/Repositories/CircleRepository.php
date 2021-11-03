28a459f2734e1a230018d0f7eca965cd987587e6
crabsinger <83605543+crabsinger@users.noreply.github.com>
2021-11-03 11:28:59 -0700

    run linter

--- app/Repositories/CircleRepository.php
+++ app/Repositories/CircleRepository.php
@@ -12,22 +12,26 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Intervention\Image\Facades\Image;
 
-class CircleRepository {
+class CircleRepository
+{
 
     protected $model;
-    public function __construct(Circle $circle) {
+    public function __construct(Circle $circle)
+    {
         $this->model = $circle;
     }
 
-    public function getCircles($request) {
+    public function getCircles($request)
+    {
         return $this->model->filter($request->all())->with('protocol')->get();
     }
 
-    public function createCircle($request) {
+    public function createCircle($request)
+    {
         $data = $request->only('address', 'user_name', 'circle_name', 'protocol_id', 'protocol_name', 'uxresearch_json');
 
-        DB::transaction(function() use ($data) { 
-            if(empty($data['protocol_id'])) {
+        DB::transaction(function () use ($data) {
+            if (empty($data['protocol_id'])) {
                 $protocol = new Protocol(['name' => $data['protocol_name']]);
                 $protocol->save();
                 $protocol_id = $protocol->id;
@@ -37,8 +41,10 @@ class CircleRepository {
 
             $address = strtolower($data['address']);
             $circle = $this->model->create(['name' => $data['circle_name'], 'protocol_id' => $protocol_id]);
-            $user = new User(['name' => $data['user_name'], 'circle_id' => $circle->id,
-                'role' => config('enums.user_types.admin'), 'address' => $address]);
+            $user = new User([
+                'name' => $data['user_name'], 'circle_id' => $circle->id,
+                'role' => config('enums.user_types.admin'), 'address' => $address
+            ]);
             $user->save();
             self::addCoordinapeUserToCircle($circle->id);
             Profile::firstOrCreate([
@@ -53,29 +59,44 @@ class CircleRepository {
         });
     }
 
-    public function updateCircle($circle, $request) {
-        $circle->update($request->only('name','token_name','team_sel_text','alloc_text','vouching',
-            'min_vouches','nomination_days_limit','vouching_text','team_selection','default_opt_in',
-            'discord_webhook','only_giver_vouch'));
+    public function updateCircle($circle, $request)
+    {
+        $circle->update($request->only(
+            'name',
+            'token_name',
+            'team_sel_text',
+            'alloc_text',
+            'vouching',
+            'min_vouches',
+            'nomination_days_limit',
+            'vouching_text',
+            'team_selection',
+            'default_opt_in',
+            'discord_webhook',
+            'only_giver_vouch'
+        ));
 
-        if(!$circle->vouching) {
+        if (!$circle->vouching) {
             $circle->nominees()->update(['ended' => 1]);
         }
 
         return $circle;
     }
 
-    public function uploadCircleLogo($request) {
+    public function uploadCircleLogo($request)
+    {
         $file = $request->file('file');
         $extension = strtolower($file->getCLientOriginalExtension()) == 'jfif' ? 'jpeg' : strtolower($file->getCLientOriginalExtension());
         $resized = Image::make($file)
-            ->resize(100, null, function ($constraint) { $constraint->aspectRatio(); } )
-            ->encode($extension,80);
-        $new_file_name = Str::slug(pathinfo(basename($file->getClientOriginalName()), PATHINFO_FILENAME)).'_'.time().'.'.$extension;
+            ->resize(100, null, function ($constraint) {
+                $constraint->aspectRatio();
+            })
+            ->encode($extension, 80);
+        $new_file_name = Str::slug(pathinfo(basename($file->getClientOriginalName()), PATHINFO_FILENAME)) . '_' . time() . '.' . $extension;
         $ret = Storage::put($new_file_name, $resized);
-        if($ret) {
+        if ($ret) {
             $circle = $request->user->circle;
-            if($circle->logo && Storage::exists($circle->logo)) {
+            if ($circle->logo && Storage::exists($circle->logo)) {
                 Storage::delete($circle->logo);
             }
             $circle->logo = $new_file_name;
@@ -86,25 +107,27 @@ class CircleRepository {
         return null;
     }
 
-    public function getWebhook($circle_id) {
+    public function getWebhook($circle_id)
+    {
         $circle = $this->model->find($circle_id);
-       return $circle->discord_webhook ?:'';
+        return $circle->discord_webhook ?: '';
     }
 
-    public function addCoordinapeUserToCircle($circle_id) {
-        $profile = Profile::firstOrCreate([ 'address' => config('ape.coordinape_user_address') ]);
+    public function addCoordinapeUserToCircle($circle_id)
+    {
+        $profile = Profile::firstOrCreate(['address' => config('ape.coordinape_user_address')]);
         $coordinape_user = new User([
-                    'address' => config('ape.coordinape_user_address'),
-                    'name' => 'Coordinape',
-                    'role' => config('enums.user_types.coordinape'),
-                    'circle_id' => $circle_id,
-                    'non_receiver' => 0,
-                    'fixed_non_receiver' => 0,
-                    'starting_tokens' => 0,
-                    'non_giver' => 1,
-                    'give_token_remaining' => 0,
-                    'bio' => "Coordinape is that the platform you’re using right now! We currently offer our service for free and invite people to allocate to us from within your circles. All funds received go towards funding the team and our operations."
-                ]);
+            'address' => config('ape.coordinape_user_address'),
+            'name' => 'Coordinape',
+            'role' => config('enums.user_types.coordinape'),
+            'circle_id' => $circle_id,
+            'non_receiver' => 0,
+            'fixed_non_receiver' => 0,
+            'starting_tokens' => 0,
+            'non_giver' => 1,
+            'give_token_remaining' => 0,
+            'bio' => "Coordinape is that the platform you’re using right now! We currently offer our service for free and invite people to allocate to us from within your circles. All funds received go towards funding the team and our operations."
+        ]);
         $coordinape_user->save();
     }
 }
