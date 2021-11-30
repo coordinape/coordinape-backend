02a370f770d480750e61aac6211762abd92b4775
Lawrence Wang <l@lw.io>
2021-11-30 10:08:41 -0800

    require timestamp in login message

--- app/Http/Middleware/VerifyLoginSignature.php
+++ app/Http/Middleware/VerifyLoginSignature.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Helper\Utils;
 use App\Models\User;
 use Closure;
+use Error;
 use Illuminate\Http\Request;
 
 class VerifyLoginSignature
@@ -18,13 +19,35 @@ class VerifyLoginSignature
      */
     public function handle(Request $request, Closure $next)
     {
+        // if there's already a valid token, continue
+        if(auth('sanctum')->check()) 
+            return $next($request);
+
         $signature = $request->get('signature');
         $address = $request->get('address');
         $hash = $request->get('hash');
-        $data = "Login to Coordinape";
+        $data = $request->get('data');
 
-        if(!Utils::validateSignature($address, $data, $signature, $hash))
-            abort(403, 'Login signature is not valid');
+        $signature_timestamp = str_replace('Login to Coordinape', '', $data);
+    
+        if (!ctype_digit(trim(strval($signature_timestamp))))
+           abort(401, 'Login signature is missing timestamp');
+
+        $ts1 = (int)$signature_timestamp;
+        $ts2 = time();
+        $seconds_diff = $ts2 - $ts1;
+
+        if ($seconds_diff > 300) // 5 minutes
+            abort(401, 'Login signature is too old');
+        
+        $valid = false;
+
+        try {
+            $valid = Utils::validateSignature($address, $data, $signature, $hash);
+        } catch (Error) {}
+
+        if (!$valid)
+            abort(401, 'Login signature is not valid');
 
         return $next($request);
     }
--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 
 // login
 Route::post('/v2/login', [ProfileController::class, 'login'])->middleware(['verify-login-sign']);
-Route::post('/v2/manifest', [ProfileController::class, 'manifest'])->middleware(['verify-token-or-sign']);
+Route::post('/v2/manifest', [ProfileController::class, 'manifest'])->middleware(['verify-login-sign']);
 
 Route::middleware(['verify-sign', 'hcaptcha-verify'])->group(function () {
     Route::post('/v2/circles', [CircleController::class, 'createCircle']);
