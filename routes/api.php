ba1030b871e9c89a0fa2ad4d988c6ee1453480c6
zashton <17747090+zashton@users.noreply.github.com>
2021-12-02 12:36:02 +0800

    remove deprecated

--- routes/api.php
+++ routes/api.php
@@ -96,58 +96,6 @@ Route::post("/" . config('telegram.token') . "/bot-update", [BotController::clas
 /********************************** V2 ENDPOINTs ***************************************************/
 
 
-/********************************** TO BE DEPRECATED ***********************************************/
-
-//Route::prefix('{circle_id}')->group(function () {
-//    Route::prefix('admin')->middleware(['verify-admin-sign'])->group(function () {
-//        Route::put('/circles/{circle}', [CircleController::class, 'updateCircle']);
-//        Route::put('/users/{address}', [UserController::class, 'adminUpdateUser']);
-//        Route::post('/users', [UserController::class, 'createUser']);
-//        Route::delete('/users/{address}', [UserController::class, 'deleteUser']);
-//        Route::post('/v2/epoches', [EpochController::class, 'createEpoch']);
-//        Route::put('/epoches/{epoch}', [EpochController::class, 'updateEpoch']);
-//        Route::delete('/epoches/{epoch}', [EpochController::class, 'deleteEpoch']);
-//        Route::post('/upload-logo', [CircleController::class, 'uploadCircleLogo']);
-//        Route::get('/webhook', [CircleController::class, 'getWebhook']);
-//
-//    });
-//    Route::middleware(['verify-sign'])->group(function () {
-//        Route::put('/users', [UserController::class, 'updateMyUser']);
-//        Route::put('/users/{address}', [UserController::class, 'updateMyUser']); // deprecated
-//        Route::post('/v2/token-gifts/{address}', [DataController::class, 'newUpdateGifts']);
-//        Route::post('/teammates', [DataController::class, 'updateTeammates']);
-//        Route::post('/nominees', [NominationController::class, 'createNominee']);
-//        Route::post('/vouch', [NominationController::class, 'addVouch']);
-//    });
-//
-//    Route::get('/circles', [CircleController::class, 'getCircles']);
-//    Route::get('/pending-token-gifts', [DataController::class, 'getPendingGifts']);
-//    Route::get('/csv', [DataController::class, 'generateCsv']);
-//    Route::get('/nominees', [NominationController::class, 'getNominees']);
-//    Route::get('/epoches', [EpochController::class, 'epoches']);
-//
-//});
-//
-//Route::middleware(['verify-sign'])->group(function () {
-//    Route::post('/upload-avatar/{address}', [ProfileController::class, 'uploadMyProfileAvatar']);
-//    Route::post('/upload-background/{address}', [ProfileController::class, 'uploadMyProfileBackground']);
-//    Route::post('/profile', [ProfileController::class, 'updateMyProfile']);
-//});
-//
-//Route::get('/profile/{address}', [ProfileController::class, 'getProfile']);
-//Route::get('/protocols', [DataController::class, 'getProtocols']);
-//Route::get('/circles', [CircleController::class, 'getCircles']);
-//Route::middleware(['hcaptcha-verify', 'verify-sign'])->group(function () {
-//    Route::post('/circles', [CircleController::class, 'createCircle']);
-//});
-//Route::get('/users/{address}', [UserController::class, 'getUser']);
-//Route::get('/users', [UserController::class, 'getUsers']);
-//Route::get('/token-gifts', [DataController::class, 'getGifts']);
-//Route::get('/pending-token-gifts', [DataController::class, 'getPendingGifts']);
-//Route::get('/active-epochs', [EpochController::class, 'getActiveEpochs']);
-
-/********************************** TO BE DEPRECATED ***********************************************/
-
 Route::fallback(function () {
     return response()->json(['message' => 'Endpoint Not Found'], 404);
 })->name('api.fallback.404');
